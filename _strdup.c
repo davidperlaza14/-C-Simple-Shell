8694d49c457b68cf45c2750fba4f0a7503ea5ec9
xARKEINx <3884@holbertonschool.com>
2022-04-13 09:39:00 -0700

    duplicate str

---
+++ _strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - Duplicate a string.
+ * @str: The string to duplicate.
+ * Return: Success result.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *result;
+
+	result = malloc(sizeof(char) * (_strlen(str) + 1));
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		result[i] = str[i];
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
